217d34847631e64656ea8bb566309419a8c502f7
Sammy Mokkenstorm <sammymokkenstorm@gmail.com>
2025-09-16 12:27:09 +0200

    Add Task 1 Problem 1

---
+++ find_centers.py
@@ -0,0 +1,26 @@
+import numpy as np
+
+def dist(x, y):
+    z = x - y
+    return np.linalg.norm(z)
+
+train_in = np.genfromtxt("train_in.csv", delimiter=",")
+train_out = np.genfromtxt("train_out.csv", delimiter=",")
+
+cloud = []
+
+for i in range(0, 10):
+    cloud.append(train_in[np.where(train_out == i)])
+
+
+center = []
+for j in range(0, 10):
+    center.append(np.mean(cloud[j], axis=0))
+
+
+distance = np.zeros((10, 10))
+for k in range(0, 10):
+    for l in range(0, 10):
+        distance[k, l] = dist(center[k], center[l])
+
+print(distance)
