a41658cc113d7eb877dca8181ebede1b2fbb8083
Finn Buck <finneganlbuck@gmail.com>
2025-09-19 14:46:40 +0200

    Clean up TSNE.py

--- TSNE.py
+++ TSNE.py
@@ -6,20 +6,8 @@ import pandas as pd
 train_in = np.genfromtxt("train_in.csv", delimiter=",")
 train_out = np.genfromtxt("train_out.csv", delimiter=",")
 
-# train_in = train_in.tolist()
-
-# print(train_in)
-# np.isnan(train_in).any(), np.isinf(train_in).any()
-# print(train_in.shape)
-
-# train_in = train_in.astype(np.float32)
-
-# print(train_in.dtype)
-
 train_in_embedded = TSNE(n_components=2, learning_rate='auto',
                   init='random', perplexity=10).fit_transform(train_in)
-
-print(train_in_embedded.shape)
 
 fig, ax = plt.subplots()
 
